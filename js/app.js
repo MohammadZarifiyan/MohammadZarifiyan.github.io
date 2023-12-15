221e4f2d171e74215a16b5920756e35325cf06b8
Mohammad Zarifiyan <mohmmadzarifiyan82@gmail.com>
2023-12-15 19:51:15 +0330

    Update variable name

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ import {calculateTheLoanInstallmentAmount, calculateTheFinalProfit} from './help
 
 const loanAmountInput = document.getElementById('loan-amount');
 const loanInterestInput = document.getElementById('loan-interest');
-const numberofInstallmentsInput = document.getElementById('number-of-installments');
+const numberOfInstallmentsInput = document.getElementById('number-of-installments');
 const profitInput = document.getElementById('profit');
 const installmentAmountParagraph = document.getElementById('installment-amount');
 const finalProfitParagraph = document.getElementById('final-profit');
@@ -14,7 +14,7 @@ function clearFormResult() {
 
 loanAmountInput.addEventListener('input', clearFormResult);
 loanInterestInput.addEventListener('input', clearFormResult);
-numberofInstallmentsInput.addEventListener('input', clearFormResult);
+numberOfInstallmentsInput.addEventListener('input', clearFormResult);
 profitInput.addEventListener('input', clearFormResult);
 
 document.getElementById('form').addEventListener('submit', function (event) {
@@ -22,14 +22,14 @@ document.getElementById('form').addEventListener('submit', function (event) {
 
     const loanAmount = parseInt(loanAmountInput.value);
     const loanInterest = parseInt(loanInterestInput.value);
-    const numberofInstallments = parseInt(numberofInstallmentsInput.value);
+    const numberOfInstallments = parseInt(numberOfInstallmentsInput.value);
     const profit = parseInt(profitInput.value);
 
-    const installmentAmount = calculateTheLoanInstallmentAmount(loanAmount, loanInterest, numberofInstallments);
-    const finalProfit = calculateTheFinalProfit(loanAmount, numberofInstallments, installmentAmount, profit);
+    const installmentAmount = calculateTheLoanInstallmentAmount(loanAmount, loanInterest, numberOfInstallments);
+    const finalProfit = calculateTheFinalProfit(loanAmount, numberOfInstallments, installmentAmount, profit);
 
     if (typeof installmentAmount === 'number' && typeof finalProfit === 'number') {
         installmentAmountParagraph.textContent = `The amount of each installment: ${installmentAmount.toLocaleString('en-US', {maximumFractionDigits: 2})}`;
-        finalProfitParagraph.textContent = `The remaining amount for you after ${numberofInstallments} months when you have paid all the installments of this loan: ${finalProfit.toLocaleString('en-US', {maximumFractionDigits: 2})}`;
+        finalProfitParagraph.textContent = `The remaining amount for you after ${numberOfInstallments} months when you have paid all the installments of this loan: ${finalProfit.toLocaleString('en-US', {maximumFractionDigits: 2})}`;
     }
 });
